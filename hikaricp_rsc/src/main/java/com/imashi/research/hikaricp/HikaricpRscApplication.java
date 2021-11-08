1f8dec616cdc70305e023d18f878337b1d7a5b82
xiongrui <m15801983447@163.com>
2021-11-08 21:16:43 +0800

    commit

---
+++ hikaricp_rsc/src/main/java/com/imashi/research/hikaricp/HikaricpRscApplication.java
@@ -0,0 +1,13 @@
+package com.imashi.research.hikaricp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikaricpRscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikaricpRscApplication.class, args);
+	}
+
+}
---
+++ hikaricp_rsc/src/test/java/com/imashi/research/hikaricp/HikaricpRscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imashi.research.hikaricp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikaricpRscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
